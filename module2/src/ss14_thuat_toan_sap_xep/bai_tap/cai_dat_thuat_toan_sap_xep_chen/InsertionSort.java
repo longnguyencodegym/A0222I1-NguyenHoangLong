ed0c248c05d3aaf84ce4cf9f31514b2ac8a21388
longnguyencodegym <longnguyen.tourism@gmail.com>
2022-06-24 21:14:25 +0700

    update_bai_tap_ngay24_thang6

---
+++ module2/src/ss14_thuat_toan_sap_xep/bai_tap/cai_dat_thuat_toan_sap_xep_chen/InsertionSort.java
@@ -0,0 +1,35 @@
+package ss14_thuat_toan_sap_xep.bai_tap.cai_dat_thuat_toan_sap_xep_chen;
+
+public class InsertionSort {
+    static int[] arr = {3, 5, 8, 2, 6, 9, 4, 1, 10};
+
+    public static void main(String[] args) {
+        insertionSort(arr);
+    }
+
+    public static void insertionSort(int[] array) {
+        System.out.println("Array before:");
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%d ",array[i]);
+        }
+        int pos, x;
+        for (int i = 1; i < array.length; i++) {
+            boolean flag = false;
+            x = array[i];
+            pos = i;
+            while (pos > 0 && x < array[pos - 1]) {
+
+                array[pos] = array[pos - 1];
+                array[pos-1] = x;
+                pos--;
+            }
+
+                }
+    System.out.println("\nArray after sort:");
+        for (int element : array) {
+            System.out.printf("%d ", element);
+            }
+
+        }
+    }
