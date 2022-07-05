19b18e9b57e5729c2242a333d8bc51796818c85d
longnguyencodegym <longnguyen.tourism@gmail.com>
2022-07-05 11:13:39 +0700

    update_bai_tap_ngay5_thang7

--- module2/src/ss17_io_binary_file_serialization/bai_tap/copy_file_nhi_phan/TestIoObject.java
+++ module2/src/ss17_io_binary_file_serialization/bai_tap/copy_file_nhi_phan/TestIoObject.java
@@ -1,5 +1,6 @@
 package ss17_io_binary_file_serialization.bai_tap.copy_file_nhi_phan;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,13 +11,16 @@ public class TestIoObject {
   public static void main(String[] args) {
     List<Country> countries=new ArrayList<>();
     countries.add(new Country(1,"84","Viet Nam"));
+    countries.add(new Country(2,"85","Laos"));
     IoObject.writeObjectFile(countries,inputLink);
     List<Country> tempCountries=IoObject.readObjectFile(inputLink);
     IoObject.writeObjectFile(tempCountries,outputLink);
+    File outputFile=new File(outputLink);
     outputCountries=IoObject.readObjectFile(outputLink);
     for (Country country :outputCountries ) {
       System.out.println(country);
     }
+    System.out.println("Byte: "+outputFile.length());
     }
 
   }
